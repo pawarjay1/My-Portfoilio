b8547b308c935e9c82fdcc821c82833ab8f677c0
pawarjay1 <jaykumarpawar08@gmail.com>
2025-06-08 14:28:30 +0530

    commit

--- Portfoilio/src/component/Layout/Header.jsx
+++ Portfoilio/src/component/Layout/Header.jsx
@@ -1,19 +1,31 @@
 import { NavLink } from 'react-router-dom';
-import styles from './Header.module.css';
+import './Header.css';
+import { useState } from 'react';
+
 export const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
     return (
         <>
             <header>
-                <div className={styles.logo}>
-                
-                </div>
-                <nav>
-                    <NavLink to="/" className={styles["nav-link"]}>About Me</NavLink>
-                    <NavLink to="/Skills" className={styles["nav-link"]}>Skills</NavLink>
-                    <NavLink to="/Projects" className={styles["nav-link"]}>Projects</NavLink>
-                    <ion-icon name="menu-outline"></ion-icon>
-                </nav>
+                <div className="logo">
 
+                </div>
+
+                <ul className={`nav-links ${isOpen ? "show" : ''}`}>
+                    <NavLink to="/" className="nav-link" onClick={closeMenu}>About Me</NavLink>
+                    <NavLink to="/Skills" className="nav-link" onClick={closeMenu}>Skills</NavLink>
+                    <NavLink to="/Projects" className="nav-link" onClick={closeMenu}>Projects</NavLink>
+                </ul>
+                <ion-icon name="menu-outline" onClick={toggleMenu}></ion-icon>
             </header>
         </>
     )
