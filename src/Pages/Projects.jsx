7fec98fdea03e6e58445e9ae35e1e2a31874bc08
pawarjay1 <jaykumarpawar08@gmail.com>
2025-06-09 13:26:38 +0530

    commit

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,3 +1,7 @@
+
+import bus from '/images/bus.png';
+import medguide from '/images/medguide.png';
+import todo from '/images/todo.png';
 export const Projects = () => {
     return (
         <>
@@ -7,29 +11,29 @@ export const Projects = () => {
 
                 <div className="skills-cards">
                     <div className="skill-card">
-                        <img src="https://private-user-images.githubusercontent.com/81229505/240140804-24693600-84b1-412a-b02c-8ea1df08c90d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDk0MDcyNDcsIm5iZiI6MTc0OTQwNjk0NywicGF0aCI6Ii84MTIyOTUwNS8yNDAxNDA4MDQtMjQ2OTM2MDAtODRiMS00MTJhLWIwMmMtOGVhMWRmMDhjOTBkLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA2MDglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwNjA4VDE4MjIyN1omWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWZhYmFkODNmNDYxZDE4Y2I0YmY5NWQ3MzNlMmI2OTU2NWU2ZmZlODNjNDcxN2I3NzY5NjE5NDhkYTIzOTI2YzgmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.UYrv0_BCUWd4iBomPQs6MH0JL0mMpJOVOuOufnP0AV4" alt="HTML logo" className="logo" />
+                        <img src={bus} alt="HTML logo" className="logo" />
                         <h3>Bus Managment system</h3>
-                        <p style={{color: "white"}}>Designed and developed a web application with separate portals for Admin, Driver, and Parents. <br/>
+                        <p style={{ color: "white" }}>Designed and developed a web application with separate portals for Admin, Driver, and Parents. <br />
                             Implemented real-time bus tracking (Geolocation API), fee payments, and user profile management.
-                        </p> 
-                        <p style={{color: "white"}}>Tech used : HTML,CSS,JavaScript,PHP,MySQL</p>
+                        </p>
+                        <p style={{ color: "white" }}>Tech used : HTML,CSS,JavaScript,PHP,MySQL</p>
                         <a href="https://github.com/pawarjay1/Bus-managment-system">GitHub</a>
                     </div>
                     <div className="skill-card">
-                        <img src="https://private-user-images.githubusercontent.com/81229505/452812833-1280011b-3142-44f4-94ef-bccf9ccb33b3.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDk0MTAxMjYsIm5iZiI6MTc0OTQwOTgyNiwicGF0aCI6Ii84MTIyOTUwNS80NTI4MTI4MzMtMTI4MDAxMWItMzE0Mi00NGY0LTk0ZWYtYmNjZjljY2IzM2IzLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA2MDglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwNjA4VDE5MTAyNlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTkxNDE0MzEyYzc5ZDI1MjA1NmMyNWFhOTQ1OGM0YmVjYWEwMmNiZmIxY2Y5ODliZmY4ODFmZjg2OWUxYjMwNWUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.fw5zmp7KaF3I8FqgqbmzCeicFpiQj20rCnvVZY6G-Pk" alt="HTML logo" className="logo" />
+                        <img src={medguide} alt="HTML logo" className="logo" />
                         <h3>medguide</h3>
-                        <p style={{color: "white"}}>Med Guide is designed to make accessing medicines and medical information quick and simple. <br/>It features a Prescription Scanner that scans prescriptions using OCR technology, identifies the medicines, and locates nearby pharmacies with stock.</p> 
-                        <p style={{color: "white"}}>Tech used : HTML,CSS,JavaScript,PHP,MySQL</p>
+                        <p style={{ color: "white" }}>Med Guide is designed to make accessing medicines and medical information quick and simple. <br />It features a Prescription Scanner that scans prescriptions using OCR technology, identifies the medicines, and locates nearby pharmacies with stock.</p>
+                        <p style={{ color: "white" }}>Tech used : HTML,CSS,JavaScript,PHP,MySQL</p>
                         <a href="https://github.com/pawarjay1/medguide">GitHub</a>
                     </div>
                     <div className="skill-card">
-                        <img src="https://private-user-images.githubusercontent.com/81229505/452813276-0a26a6b3-0cc8-4cf5-bd6c-8c2959fa0b92.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDk0MTA1NjIsIm5iZiI6MTc0OTQxMDI2MiwicGF0aCI6Ii84MTIyOTUwNS80NTI4MTMyNzYtMGEyNmE2YjMtMGNjOC00Y2Y1LWJkNmMtOGMyOTU5ZmEwYjkyLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA2MDglMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwNjA4VDE5MTc0MlomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTQyOWMyNTZlODY5YzExZjBmNzZkNGNlZWVlOGI4ZTNkNDY1NzE4N2NmNjI2MjczODc3YzZlYTFkNDY1MzlkNDcmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.MxQWg794GjbCyKOkc1QkPOaWhofog_mF-XsgzUjKOjo" alt="HTML logo" className="logo" />
+                        <img src={todo} alt="HTML logo" className="logo" />
                         <h3>ToDo App</h3>
-                        <p style={{color: "white"}}>A lightweight Todo App with add, edit, and double-tap delete features. <br/>
-Tasks are saved locally using Local Storage, ensuring data persistence without the need for a server. <br/>
-The app is designed with a clean and responsive UI.</p> 
-                        <p style={{color: "white"}}>Tech used : HTML,CSS,JavaScript,LocalStorage</p>
-                        <a href="https://github.com/pawarjay1/medguide">GitHub</a>
+                        <p style={{ color: "white" }}>A lightweight Todo App with add, edit, and double-tap delete features. <br />
+                            Tasks are saved locally using Local Storage, ensuring data persistence without the need for a server. <br />
+                            The app is designed with a clean and responsive UI.</p>
+                        <p style={{ color: "white" }}>Tech used : HTML,CSS,JavaScript,LocalStorage</p>
+                        <a href="https://github.com/pawarjay1/todo-in-js.github.io">GitHub</a>
                     </div>
                 </div>
             </div>
